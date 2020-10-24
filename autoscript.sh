a6c746d9cf0b116fbc25505311a63a528ccc423a
davidfrosty0001 <37801186+davidfrosty0001@users.noreply.github.com>
2020-10-24 21:07:17 +0100

    Update autoscript.sh

--- autoscript.sh
+++ autoscript.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 sudo apt update
 sudo apt install openvpn
-wget -P ~/ https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.4/EasyRSA-3.0.4.tgz
+wget -P ~/ https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.8/EasyRSA-3.0.8.tgz
 cd ~
-tar xvf EasyRSA-3.0.4.tgz
-cd ~/EasyRSA-3.0.4/
+tar xvf EasyRSA-3.0.8.tgz
+cd ~/EasyRSA-3.0.8/
 cp vars.example vars
 nano vars
 *
@@ -15,12 +15,12 @@ nano vars
 *
 *
 *
-cd EasyRSA-3.0.4/
+cd EasyRSA-3.0.8/
 ./easyrsa init-pki
 ./easyrsa gen-req server nopass
-sudo cp ~/EasyRSA-3.0.4/pki/private/server.key /etc/openvpn/
-cp ~/EasyRSA-3.0.4/pki/reqs/server.req /tmp
-cd EasyRSA-3.0.4/
+sudo cp ~/EasyRSA-3.0.8/pki/private/server.key /etc/openvpn/
+cp ~/EasyRSA-3.0.8/pki/reqs/server.req /tmp
+cd EasyRSA-3.0.8/
 ./easyrsa import-req /tmp/server.req server
 ./easyrsa sign-req server server
 *
@@ -29,18 +29,18 @@ cd EasyRSA-3.0.4/
 cp pki/issued/server.crt /tmp
 cp pki/ca.crt /tmp
 sudo cp /tmp/{server.crt,ca.crt} /etc/openvpn/
-cd EasyRSA-3.0.4/
+cd EasyRSA-3.0.8/
 ./easyrsa gen-dh
 openvpn --genkey --secret ta.key
-sudo cp ~/EasyRSA-3.0.4/ta.key /etc/openvpn/
-sudo cp ~/EasyRSA-3.0.4/pki/dh.pem /etc/openvpn/
+sudo cp ~/EasyRSA-3.0.8/ta.key /etc/openvpn/
+sudo cp ~/EasyRSA-3.0.8/pki/dh.pem /etc/openvpn/
 mkdir -p ~/client-configs/keys
 chmod -R 700 ~/client-configs
-cd ~/EasyRSA-3.0.4/
+cd ~/EasyRSA-3.0.8/
 ./easyrsa gen-req client1 nopass
 cp pki/private/client1.key ~/client-configs/keys/
 cp pki/reqs/client1.req /tmp
-cd EasyRSA-3.0.4/
+cd EasyRSA-3.0.8/
 ./easyrsa import-req /tmp/client1.req client1
 ./easyrsa sign-req client client1
 *
@@ -48,7 +48,7 @@ cd EasyRSA-3.0.4/
 *
 cp pki/issued/client1.crt /tmp
 cp /tmp/client1.crt ~/client-configs/keys/
-cp ~/EasyRSA-3.0.4/ta.key ~/client-configs/keys/
+cp ~/EasyRSA-3.0.8/ta.key ~/client-configs/keys/
 sudo cp /etc/openvpn/ca.crt ~/client-configs/keys/
 sudo mkdir -p /etc/openvpn/
 sudo wget https://raw.githubusercontent.com/davidfrosty0001/ovpn-port-forward/master/server.conf
